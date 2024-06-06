0b425b0a3d75df9eb779770026b0102a31c97de6
ShoriuOfficial <155915828+ShoriuOfficial@users.noreply.github.com>
2024-06-06 12:22:07 +0400

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,9 @@
+public class HW1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 45; i++) {
+            if (i % 7 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
