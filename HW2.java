5f6458c85243cdf1087dcdc3341201f56c8d76b2
ShoriuOfficial <155915828+ShoriuOfficial@users.noreply.github.com>
2024-06-12 19:03:26 +0400

    Add files via upload

---
+++ HW2.java
@@ -0,0 +1,10 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+        for (int a = 95; a <= 17; a--) {
+            System.out.println(a);
+        }
+    }
+
+}
+
---
+++ HWW.java
@@ -0,0 +1,11 @@
+public class HWW {
+
+    public static void main(String[] args) {
+        for (int a = 0; a <= 50; a += 0.1) {
+
+            System.out.println(a);
+
+        }
+
+    }
+}
