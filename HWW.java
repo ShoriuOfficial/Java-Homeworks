cdf66bf67a859509bcfe082acd4ccc213856670f
ShoriuOfficial <155915828+ShoriuOfficial@users.noreply.github.com>
2024-06-12 19:08:09 +0400

    Update HWW.java

--- HWW.java
+++ HWW.java
@@ -1,7 +1,7 @@
 public class HWW {
 
     public static void main(String[] args) {
-        for (int a = 0; a <= 50; a += 0.1) {
+        for (int a = 0; a >= 50; a += 0.1) {
 
             System.out.println(a);
 
